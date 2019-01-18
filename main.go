2464e508f403ab546fbb67fb507167422fcfc1ec
Bryan Calvo <bryancalvobenoit@gmail.com>
2019-01-18 01:40:13 -0400

    Create new client associated with a providers by ID

--- main.go
+++ main.go
@@ -7,9 +7,24 @@ import (
 	"os"
 )
 
-type provider struct {
+type Provider struct {
 	ID   int32
 	Name string
+}
+
+type Client struct {
+	ID         int32
+	Name       string
+	providerID int
+}
+
+func newClient(id int32, name string, provider int) {
+	cliente := Client{ID: id, Name: name, providerID: provider}
+	jclient, _ := json.Marshal(cliente)
+	f, err := os.OpenFile("data/clients.json", os.O_APPEND, os.ModeAppend)
+	check(err)
+	defer f.Close()
+	f.WriteString(string(jclient))
 }
 
 func check(e error) {
@@ -18,24 +33,25 @@ func check(e error) {
 	}
 }
 
-func newProvider(id int32, name string) {
-	prov := provider{ID: id, Name: name}
+func newProvider(id int32, name string) Provider {
+	prov := Provider{ID: id, Name: name}
 	jprov, _ := json.Marshal(prov)
 	f, err := os.OpenFile("data/providers.json", os.O_APPEND, os.ModeAppend)
 	check(err)
 	defer f.Close()
 	f.WriteString(string(jprov))
+	return prov
 }
 
 func listProviders() {
 	r, err := ioutil.ReadFile("data/providers.json")
 	check(err)
-	fmt.Println(r)
-
+	fmt.Println(string(r))
 }
 
 func main() {
+
 	newProvider(1, "Mateo")
-	newProvider(2, "Marcos")
 	listProviders()
+	newClient(1, "Miguel", 1)
 }
