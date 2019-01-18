3b7a0559deecb5d3741801367e8b0b0ecf6c18bb
Bryan Calvo <bryancalvobenoit@gmail.com>
2019-01-18 02:03:32 -0400

    Loan creation completed

--- main.go
+++ main.go
@@ -15,11 +15,24 @@ type Provider struct {
 type Client struct {
 	ID         int32
 	Name       string
-	providerID int
+	ProviderID int
 }
 
-func newClient(id int32, name string, provider int) {
-	cliente := Client{ID: id, Name: name, providerID: provider}
+type Loan struct {
+	ID         int32
+	Balance    float64
+	ProviderID int32
+	ClientID   int32
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func newClient(id int32, name string, idnumber int) {
+	cliente := Client{ID: id, Name: name, ProviderID: idnumber}
 	jclient, _ := json.Marshal(cliente)
 	f, err := os.OpenFile("data/clients.json", os.O_APPEND, os.ModeAppend)
 	check(err)
@@ -27,10 +40,13 @@ func newClient(id int32, name string, provider int) {
 	f.WriteString(string(jclient))
 }
 
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
+func newLoan(id int32, balance float64, provid int32, cliID int32) {
+	loan := Loan{ID: id, Balance: balance, ProviderID: provid, ClientID: cliID}
+	jloan, _ := json.Marshal(loan)
+	f, err := os.OpenFile("data/loans.json", os.O_APPEND, os.ModeAppend)
+	check(err)
+	defer f.Close()
+	f.WriteString(string(jloan))
 }
 
 func newProvider(id int32, name string) Provider {
@@ -53,5 +69,6 @@ func main() {
 
 	newProvider(1, "Mateo")
 	listProviders()
-	newClient(1, "Miguel", 1)
+	newClient(1, "Miguel", 112)
+	newLoan(1, 5000, 1, 112)
 }
