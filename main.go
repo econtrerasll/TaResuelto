f3d51790f92759235fba8b55157d8e5947593eb3
bryan.calvo <bryan.calvo@wirecardlatam.lan>
2019-01-18 10:09:24 -0400

    Adding entity creation functions

--- main.go
+++ main.go
@@ -7,9 +7,22 @@ import (
 	"os"
 )
 
-type provider struct {
+type Provider struct {
 	ID   int32
 	Name string
+}
+
+type Client struct {
+	ID         int32
+	Name       string
+	ProviderID int32
+}
+
+type Loan struct {
+	ID        int32
+	Balance   float64
+	OwnerID   int32
+	CreatorID int32
 }
 
 func check(e error) {
@@ -19,12 +32,30 @@ func check(e error) {
 }
 
 func newProvider(id int32, name string) {
-	prov := provider{ID: id, Name: name}
+	prov := Provider{ID: id, Name: name}
 	jprov, _ := json.Marshal(prov)
 	f, err := os.OpenFile("data/providers.json", os.O_APPEND, os.ModeAppend)
 	check(err)
 	defer f.Close()
 	f.WriteString(string(jprov))
+}
+
+func newClient(id int32, name string, provID int32) {
+	cli := Client{ID: id, Name: name, ProviderID: provID}
+	jcli, _ := json.Marshal(cli)
+	f, err := os.OpenFile("data/clients.json", os.O_APPEND, os.ModeAppend)
+	check(err)
+	defer f.Close()
+	f.WriteString(string(jcli))
+}
+
+func newLoan(id int32, balance float64, creatorID int32, ownerID int32) {
+	loan := Loan{ID: id, Balance: balance, CreatorID: creatorID, OwnerID: ownerID}
+	jloan, _ := json.Marshal(loan)
+	f, err := os.OpenFile("data/loans.json", os.O_APPEND, os.ModeAppend)
+	check(err)
+	defer f.Close()
+	f.WriteString(string(jloan))
 }
 
 func listProviders() {
